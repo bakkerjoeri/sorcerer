36768bd85722368ed58370a85f6cd4a9dd69c7b9
Joeri Bakker <bakkerjoeri@gmail.com>
2017-08-18 10:51:53 +0200

    Moved entity and sprite into core

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,5 @@
+import Game from './Game';
+
 require('../style/main.scss');
 
-import Entity from './Entity';
-import Sprite from './Sprite';
-
-const canvas = document.querySelector('.canvas__sorcerer');
-const context = canvas.getContext('2d');
-
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-
-let playerImage = new Image();
-playerImage.src = '../assets/character.png';
-
-let playerEntity = new Entity({
-	sprite: new Sprite(playerImage, 16, 16),
-	position: {
-		x: 16,
-		y: 16,
-	},
-});
-
-playerEntity.draw(context);
+new Game();
