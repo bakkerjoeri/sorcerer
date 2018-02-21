145911c76cf1b17ad1a3ff573ceb264ff57463d9
Joeri Bakker <bakkerjoeri@gmail.com>
2018-02-21 17:38:46 +0100

    Plan some random movements

--- script/goal/Wander.js
+++ script/goal/Wander.js
@@ -5,47 +5,29 @@ import MoveSouth from 'goal/MoveSouth';
 import MoveWest from 'goal/MoveWest';
 
 export default class Wander extends Goal {
-	takeAction(actor) {
+	takeAction() {
 		return new Promise((success) => {
-			let direction = Math.round(Math.random() * 3);
-			// let howManyTimes = Math.round(Math.random() * 2) + 1;
+			let howManyTimes = Math.round(Math.random() * 3) + 1;
 
-			// for(let t = 0; t < howManyTimes; t++) {
+			for(let t = 0; t < howManyTimes; t++) {
+				let direction = Math.round(Math.random() * 3);
+
 				if (direction === 0) {
-					console.log(`${actor.type} will make a clockwise square`);
 					this.subGoals.push(new MoveNorth(this));
-					this.subGoals.push(new MoveEast(this));
-					this.subGoals.push(new MoveSouth(this));
-					this.subGoals.push(new MoveWest(this));
 				}
 
-				// move right, left, left, right
 				if (direction === 1) {
-					console.log(`${actor.type} will wiggle horizontally`);
-					this.subGoals.push(new MoveEast(this));
-					this.subGoals.push(new MoveWest(this));
-					this.subGoals.push(new MoveWest(this));
 					this.subGoals.push(new MoveEast(this));
 				}
 
-				// make a square, counter clockwise
 				if (direction === 2) {
-					console.log(`${actor.type} will make a counter-clockwise square`);
-					this.subGoals.push(new MoveNorth(this));
-					this.subGoals.push(new MoveWest(this));
 					this.subGoals.push(new MoveSouth(this));
-					this.subGoals.push(new MoveEast(this));
 				}
 
-				// move up, down, down, up.
 				if (direction === 3) {
-					console.log(`${actor.type} will wiggle vertically`);
-					this.subGoals.push(new MoveNorth(this));
-					this.subGoals.push(new MoveSouth(this));
-					this.subGoals.push(new MoveSouth(this));
-					this.subGoals.push(new MoveNorth(this));
+					this.subGoals.push(new MoveWest(this));
 				}
-			// }
+			}
 
 			success();
 		});
