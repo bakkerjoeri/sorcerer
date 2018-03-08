d286e59f3ba64d283667e4beb971bbe6fb9abdc3
Joeri Bakker <bakkerjoeri@gmail.com>
2018-03-08 23:30:43 +0100

    Floodfill using Dijkstra to find a position to move to

--- script/goal/Wander.js
+++ script/goal/Wander.js
@@ -1,5 +1,7 @@
 import Goal from 'module/Goal';
 import MoveToPosition from 'goal/MoveToPosition';
+import DijkstraMap from 'module/DijkstraMap';
+import choose from 'random/choose';
 
 export default class Wander extends Goal {
 	takeAction(actor) {
@@ -9,10 +11,14 @@ export default class Wander extends Goal {
 				return succeed();
 			}
 			
-			this.subGoals.push(new MoveToPosition({
-				x: Math.floor(Math.random() * actor.level.size.width),
-				y: Math.floor(Math.random() * actor.level.size.height),
-			}, this));
+			let possiblePositions = this.findPossiblePositionsInLevelFromStartingPosition(actor.level, actor.positionInLevel);
+			
+			if (possiblePositions.length === 0) {
+				actor.wait();
+				return succeed();
+			}
+			
+			this.subGoals.push(new MoveToPosition(choose(possiblePositions), this));
 			
 			return succeed();
 		});
@@ -20,5 +26,36 @@ export default class Wander extends Goal {
 
 	isFinished() {
 		return false;
+	}
+	
+	findPossiblePositionsInLevelFromStartingPosition(level, startingPosition) {
+		let possiblePositions = [];
+		
+		// calculate dijkstra map for position
+		let dijkstraMap = new DijkstraMap(level.size);
+		dijkstraMap.findCellAtPosition(startingPosition).setAsTarget();
+		
+		let solidEntities = level.getSolidEntitiesInBoundaries({x: 0, y: 0}, level.size);
+		solidEntities.forEach((solidEntity) => {
+			if (solidEntity.positionInLevel.x !== startingPosition.x || solidEntity.positionInLevel.y !== startingPosition.y) {
+				dijkstraMap.findCellAtPosition(solidEntity.positionInLevel).setPassability(false);
+			}
+		});
+		
+		dijkstraMap.draw();
+		dijkstraMap.calculate();
+		dijkstraMap.draw();
+		
+		// Check if the starting position is part of the possible positions.
+		if (dijkstraMap.findCellAtPosition(startingPosition).weight !== Infinity) {
+			// Flood fill to find all possible positions.
+			dijkstraMap.forEachCell((cell) => {
+				if (cell.weight !== Infinity && cell.passable) {
+					possiblePositions.push(cell.position);
+				}
+			});
+		}
+		
+		return possiblePositions;
 	}
 }
