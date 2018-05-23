2c9f8d665369ea9057ed7c95e557e3b561bc5abf
Joeri Bakker <bakkerjoeri@gmail.com>
2018-05-23 09:27:02 +0200

    Add gameObject reducers and actions

--- script/core/reducers/index.js
+++ script/core/reducers/index.js
@@ -1,8 +1,19 @@
-import {ADD_VIEWPORT, CHANGE_VIEWPORT_POSITION, SET_VIEWPORT_IS_ACTIVE} from './../actions/viewports';
+import combineReducers from './../combineReducers';
+import {
+	ADD_VIEWPORT,
+	CHANGE_VIEWPORT_POSITION,
+	SET_VIEWPORT_IS_ACTIVE
+} from './../actions/viewports';
+import {
+	ADD_GAME_OBJECT,
+} from './../actions/gameObjects';
 
 const initialState = {
 	viewports: {},
+	gameObjects: {},
 };
+
+export default combineReducers(viewports, gameObjects);
 
 export function viewports(state = initialState, action) {
 	switch (action.type) {
@@ -26,6 +37,19 @@ export function viewports(state = initialState, action) {
 					[action.id]: Object.assign({}, state.viewports[action.id], {
 						position: action.position,
 					}),
+				}),
+			});
+		default:
+			return state;
+	}
+}
+
+export function gameObjects(state = initialState, action) {
+	switch (action.type) {
+		case ADD_GAME_OBJECT:
+			return Object.assign({}, state, {
+				gameObjects: Object.assign({}, state.gameObjects, {
+					[action.gameObject.id]: action.gameObject,
 				}),
 			});
 		default:
