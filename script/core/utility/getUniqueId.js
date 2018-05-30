03f3449bf71bf9be2fd634983f655c74fca70970
Joeri Bakker <bakkerjoeri@gmail.com>
2018-05-30 21:38:59 +0200

    Give id as string

--- script/core/utility/getUniqueId.js
+++ script/core/utility/getUniqueId.js
@@ -1,8 +1,8 @@
 let nextId = 0;
 
-export default function getUniqueId() {
+export default function getUniqueId(name) {
 	let uniqueId = nextId;
 	nextId = nextId + 1;
 
-	return uniqueId;
+	return `${uniqueId}`;
 }
