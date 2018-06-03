8c893aa15f29182a1ea9ef3266dfb7a2989a4dbf
Joeri Bakker <bakkerjoeri@gmail.com>
2018-06-03 20:35:44 +0200

    Add selector with id

--- script/library/core/model/selectors/gameObjects.js
+++ script/library/core/model/selectors/gameObjects.js
@@ -2,8 +2,12 @@ export function getGameObjects(state) {
 	return Object.values(state.gameObjects);
 }
 
+export function getGameObjectWithId(state, gameObjectId) {
+	return state.gameObjects[gameObjectId];
+}
+
 export function getGameObjectsInRoomWithId(state, roomId) {
-	return state.rooms[roomId].gameObjects.map(gameObjectId => state.gameObjects[gameObjectId]);
+	return state.rooms[roomId].gameObjects.map(gameObjectId => getGameObjectWithId(state, gameObjectId));
 }
 
 export function getVisibleGameObjectsInRoomWithId(state, roomId) {
