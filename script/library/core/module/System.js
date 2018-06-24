87115b38071d67be62c8ed89f327f6e4e8158fd9
Joeri Bakker <bakkerjoeri@gmail.com>
2018-06-25 00:10:39 +0200

    Check if updateCallback is a function

--- script/library/core/module/System.js
+++ script/library/core/module/System.js
@@ -3,8 +3,13 @@ import {getComponentsForEntityWithId} from './Component';
 import gameStateStore from './../model/gameStateStore';
 
 export default class System {
-	constructor(requiredComponents = [], updateCallback) {
+	constructor(requiredComponents = [], updateCallback = () => {}) {
 		this.requiredComponents = requiredComponents;
+
+		if (typeof updateCallback !== 'function') {
+			throw new Error(`Expected updateCallback to be of type 'function', but got '${typeof updateCallback}'.`)
+		}
+
 		this.updateCallback = updateCallback;
 	}
 
