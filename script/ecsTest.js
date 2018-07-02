6e3701b68f0813f2599040b4564f9af265d64bff
Joeri Bakker <bakkerjoeri@gmail.com>
2018-07-02 09:51:49 +0200

    Create a multitude of slimes

--- script/ecsTest.js
+++ script/ecsTest.js
@@ -10,16 +10,20 @@ import PlayerActionSystem from './system/PlayerActionSystem';
 import ActionTickerSystem from './system/ActionTickerSystem';
 import ActionSystem from './system/ActionSystem';
 
+import isPositionFree from './isPositionFree';
+
 loadSprites();
 
 let playerEntity = createEntity();
 gameStateStore.dispatch(addEntity(playerEntity));
+gameStateStore.dispatch(addComponentToEntity(playerEntity.id, 'name', 'Green Knight'));
+gameStateStore.dispatch(addComponentToEntity(playerEntity.id, 'isSolid', true));
 gameStateStore.dispatch(addComponentToEntity(playerEntity.id, 'health', {
 	current: 20, maximum: 20.
 }));
 
 gameStateStore.dispatch(addComponentToEntity(playerEntity.id, 'actionTicker', {
-	ticks: 100,
+	ticks: 0,
 }));
 
 gameStateStore.dispatch(addComponentToEntity(playerEntity.id, 'sprite', {
@@ -34,9 +38,43 @@ gameStateStore.dispatch(addComponentToEntity(playerEntity.id, 'player', true));
 gameStateStore.dispatch(addComponentToEntity(playerEntity.id, 'actor', true));
 gameStateStore.dispatch(addComponentToEntity(playerEntity.id, 'position', {x: 16, y: 16}));
 
+let amountOfSlimesToCreate = 50;
+
+while(amountOfSlimesToCreate > 0) {
+	let position = {x: Math.floor(Math.random() * 20) * 16, y: Math.floor(Math.random() * 20) * 16};
+
+	if (isPositionFree(position)) {
+		let slimeEntity = createEntity();
+		gameStateStore.dispatch(addEntity(slimeEntity));
+		gameStateStore.dispatch(addComponentToEntity(slimeEntity.id, 'name', 'Slime'));
+		gameStateStore.dispatch(addComponentToEntity(slimeEntity.id, 'isSolid', true));
+		gameStateStore.dispatch(addComponentToEntity(slimeEntity.id, 'health', {
+			current: 10, maximum: 10,
+		}));
+
+		gameStateStore.dispatch(addComponentToEntity(slimeEntity.id, 'actionTicker', {
+			ticks: 0,
+		}));
+
+		gameStateStore.dispatch(addComponentToEntity(slimeEntity.id, 'sprite', {
+			assetId: 'slime',
+			currentFrameIndex: 0,
+			framesPerSecond: 1,
+			isAnimationPaused: false,
+			isAnimationLooping: true,
+		}));
+
+		gameStateStore.dispatch(addComponentToEntity(slimeEntity.id, 'actor', true));
+		gameStateStore.dispatch(addComponentToEntity(slimeEntity.id, 'brain', {}));
+		gameStateStore.dispatch(addComponentToEntity(slimeEntity.id, 'position', position));
+
+		amountOfSlimesToCreate -= 1;
+	}
+}
+
 let game = new Game(document.querySelector('.canvas__sorcerer'), 4);
 
-// game.addSystem(new AnimationSystem());
+game.addSystem(new AnimationSystem());
 game.addSystem(new RenderSystem());
 game.addSystem(new PlayerActionSystem());
 game.addSystem(new ActionSystem());
