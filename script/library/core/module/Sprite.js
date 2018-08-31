14d528288ec092d76744fd106868102e8d5c0cc0
Joeri Bakker <bakkerjoeri@gmail.com>
2018-08-31 09:49:10 +0200

    Add offset to sprite

--- script/library/core/module/Sprite.js
+++ script/library/core/module/Sprite.js
@@ -4,6 +4,10 @@ import {addSprite} from './../model/sprites';
 export function createSprite(properties = {}) {
 	const DEFAULT_PROPERTIES = {
 		spriteFrames: [],
+		offset: {
+			x: 0,
+			y: 0,
+		},
 	};
 
 	let sprite = createStateEntity('sprite', {
