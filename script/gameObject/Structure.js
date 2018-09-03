b75735c0cef23743d27758e8d19b21d3b02eba6d
Joeri Bakker <bakkerjoeri@gmail.com>
2018-09-03 19:52:39 +0200

    Correct name of constant

--- script/gameObject/Structure.js
+++ script/gameObject/Structure.js
@@ -1,11 +1,11 @@
 import Entity from './Entity';
 import HealthComponent from './../component/HealthComponent';
 
-export const ACTOR_COMPONENTS_BLUEPRINT = {};
+export const STRUCTURE_BLUEPRINTS = {};
 
 export default function Structure(components = {}) {
 	return new Entity({
-		...ACTOR_COMPONENTS_BLUEPRINT,
+		...STRUCTURE_BLUEPRINTS,
 		...components,
 	});
 }
