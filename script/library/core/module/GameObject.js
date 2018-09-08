be7f1d8edfcbf70dfda9cdf4b2dfa0c9f1e132ae
Joeri Bakker <bakkerjoeri@gmail.com>
2018-09-08 13:08:04 +0200

    Removed unused dependencies

--- script/library/core/module/GameObject.js
+++ script/library/core/module/GameObject.js
@@ -1,7 +1,5 @@
 import createStateEntity, {StateEntity} from './../utility/createStateEntity';
 import {addGameObject} from './../model/gameObjects';
-import {addGameObjectToRoom} from './../model/rooms';
-import {getCurrentRoomId} from './../model/game';
 
 export function createGameObject(components = {}) {
 	let stateEntity = createStateEntity('gameObject', {
