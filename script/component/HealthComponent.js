dcd3c5b00506d69f4129cf76b2841500615e2426
Joeri Bakker <bakkerjoeri@gmail.com>
2018-09-08 15:18:40 +0200

    Added a relevant test

--- script/component/HealthComponent.js
+++ script/component/HealthComponent.js
@@ -3,8 +3,9 @@ export const HEALTH_COMPONENT_BLUEPRINT = {
 	maximum: 1,
 }
 
-export default function HealthComponent(properties) {
-	// If no current health was given, we assume it starts out equal to maximum health.
+export default function HealthComponent(properties = {}) {
+	// If no current health is given, current health will be equal to the
+	// provided maximum health.
 	if (!properties.hasOwnProperty('current') && properties.hasOwnProperty('maximum')) {
 		properties.current = properties.maximum;
 	}
