205b74adfd38000a8097c00a95cd330815dd03c3
Joeri Bakker <bakkerjoeri@gmail.com>
2018-09-08 13:08:47 +0200

    Fixed structure blueprint constant name

--- script/gameObject/Structure.js
+++ script/gameObject/Structure.js
@@ -1,11 +1,11 @@
 import Entity from './Entity';
 import HealthComponent from './../component/HealthComponent';
 
-export const STRUCTURE_BLUEPRINTS = {};
+export const STRUCTURE_COMPONENTS_BLUEPRINT = {};
 
 export default function Structure(components = {}) {
 	return new Entity({
-		...STRUCTURE_BLUEPRINTS,
+		...STRUCTURE_COMPONENTS_BLUEPRINT,
 		...components,
 	});
 }
