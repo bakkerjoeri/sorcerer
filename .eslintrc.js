ed5baaa18a8ab07055a1f464b99438aa5d21e477
Joeri Bakker <bakkerjoeri@gmail.com>
2018-09-23 19:08:52 +0200

    Add some linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,19 @@
 module.exports = {
 	"parser": "babel-eslint",
 	"env": {
+		"browser": true,
 		"es6": true,
 	},
+	"extends": [
+		"eslint:recommended",
+	],
+	"rules": {
+		"quotes": [
+			"warn",
+			"single",
+		],
+		"no-var": "warn",
+		"prefer-template": "warn",
+		"eqeqeq": "error",
+	}
 }
