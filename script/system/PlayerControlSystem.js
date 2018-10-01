219bd61c536bca58e7dd4b0f094d5e4a77c02a3d
Joeri Bakker <bakkerjoeri@gmail.com>
2018-10-01 23:19:27 +0200

    Listen to keyboard events

--- script/system/PlayerControlSystem.js
+++ script/system/PlayerControlSystem.js
@@ -1,90 +1,55 @@
 import System from './../library/core/module/System';
-import {isKeyPressed} from './../library/core/module/Keyboard';
 import {doesGameObjectHaveComponents} from './../library/core/module/GameObject';
-
-let waitingToConclude = false;
-let hasPressedSpace = false;
-let hasMovedUp = false;
-let hasMovedRight = false;
-let hasMovedDown = false;
-let hasMovedLeft = false;
 
 export default class PlayerControlSystem extends System {
 	constructor() {
-		super(entity => doesGameObjectHaveComponents(entity, ['actor', 'player', 'canAct', 'positionInLevel']));
+		super(entity => {
+			return doesGameObjectHaveComponents(entity, ['actor', 'player', 'canAct', 'positionInLevel'])
+				&& !doesGameObjectHaveComponents(entity, ['isDead']);
+		});
 
 		this.attemptAction = this.attemptAction.bind(this);
 
-		this.observe('update', (gameObjects) => {
-			gameObjects.forEach(this.attemptAction);
+		this.observe('keydown', (gameObjects, key) => {
+			gameObjects.forEach((gameObject) => {
+				this.attemptAction(gameObject, key)
+			});
 		});
 	}
 
-	attemptAction(gameObject) {
-		let {isDead, positionInLevel} = gameObject.components;
+	attemptAction(gameObject, key) {
+		let {positionInLevel} = gameObject.components;
 
-		if (isDead && !waitingToConclude) {
-			waitingToConclude = true;
-
-			window.setTimeout(() => {
-				this.game.notify('actWait', [gameObject]);
-				waitingToConclude = false;
-			}, 1000);
-
-			return;
+		if (key === ' ') {
+			this.game.notify('actWait', [gameObject]);
 		}
 
-		if (!isDead) {
-			if (isKeyPressed(' ') && !hasPressedSpace) {
-				hasPressedSpace = true;
-				this.game.notify('actWait', [gameObject]);
-			} else if (!isKeyPressed(' ') && hasPressedSpace) {
-				hasPressedSpace = false;
-			}
+		if (key === 'ArrowUp') {
+			this.game.notify('actTowardsPosition', [gameObject], {
+				x: positionInLevel.x,
+				y: positionInLevel.y - 1,
+			});
+		}
 
-			if (isKeyPressed('ArrowUp') && !hasMovedUp) {
-				hasMovedUp = true;
+		if (key === 'ArrowRight') {
+			this.game.notify('actTowardsPosition', [gameObject], {
+				x: positionInLevel.x + 1,
+				y: positionInLevel.y,
+			});
+		}
 
-				this.game.notify('actTowardsPosition', [gameObject], {
-					x: positionInLevel.x,
-					y: positionInLevel.y - 1,
-				});
-			} else if (!isKeyPressed('ArrowUp') && hasMovedUp) {
-				hasMovedUp = false;
-			}
+		if (key === 'ArrowDown') {
+			this.game.notify('actTowardsPosition', [gameObject], {
+				x: positionInLevel.x,
+				y: positionInLevel.y + 1,
+			});
+		}
 
-			if (isKeyPressed('ArrowRight') && !hasMovedRight) {
-				hasMovedRight = true;
-
-				this.game.notify('actTowardsPosition', [gameObject], {
-					x: positionInLevel.x + 1,
-					y: positionInLevel.y,
-				});
-			} else if (!isKeyPressed('ArrowRight') && hasMovedRight) {
-				hasMovedRight = false;
-			}
-
-			if (isKeyPressed('ArrowDown') && !hasMovedDown) {
-				hasMovedDown = true;
-
-				this.game.notify('actTowardsPosition', [gameObject], {
-					x: positionInLevel.x,
-					y: positionInLevel.y + 1,
-				});
-			} else if (!isKeyPressed('ArrowDown') && hasMovedDown) {
-				hasMovedDown = false;
-			}
-
-			if (isKeyPressed('ArrowLeft') && !hasMovedLeft) {
-				hasMovedLeft = true;
-
-				this.game.notify('actTowardsPosition', [gameObject], {
-					x: positionInLevel.x - 1,
-					y: positionInLevel.y,
-				});
-			} else if (!isKeyPressed('ArrowLeft') && hasMovedLeft) {
-				hasMovedLeft = false;
-			}
+		if (key === 'ArrowLeft') {
+			this.game.notify('actTowardsPosition', [gameObject], {
+				x: positionInLevel.x - 1,
+				y: positionInLevel.y,
+			});
 		}
 	}
 }
