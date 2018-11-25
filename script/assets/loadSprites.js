d51f0c3727d30ae13f03562fe12db6e963932e8b
Joeri Bakker <bakkerjoeri@gmail.com>
2018-11-25 23:28:33 +0100

    Refactored sprite loading into an index file

--- script/assets/loadSprites.js
+++ script/assets/loadSprites.js
@@ -2,93 +2,81 @@ import {loadSpriteFrames} from './../library/core/module/SpriteFrame.js';
 import {createSprite} from './../library/core/module/Sprite.js';
 
 export default function loadSprites() {
-	loadGreenKnightSprite();
-	loadSlimeSprite();
-	loadGiantSlimeSprite();
-	loadWallSprite();
-	loadGraveSprite();
-}
-
-function loadGreenKnightSprite() {
-	loadSpriteFrames('greenknight', '/assets/images/greenknight.png', {
-		width: 16,
-		height: 16,
-	}, 0, 6);
-
-	createSprite({
-		id: 'greenknight',
-		spriteFrames: [
-			'greenknight_0',
-			'greenknight_0',
-			'greenknight_0',
-			'greenknight_0',
-			'greenknight_0',
-			'greenknight_1',
-			'greenknight_2',
-			'greenknight_3',
-			'greenknight_3',
-			'greenknight_3',
-			'greenknight_3',
-			'greenknight_4',
-			'greenknight_5',
-		],
+	spriteIndex.forEach((spriteData) => {
+		loadSpriteFrames(spriteData.name, spriteData.filepath, spriteData.frameSize, spriteData.frameStart, spriteData.frameOffset);
+		createSprite({
+			id: spriteData.name,
+			spriteFrames: spriteData.frames.map((frameId) => `${spriteData.name}_${frameId}`),
+		});
 	});
 }
 
-function loadSlimeSprite() {
-	loadSpriteFrames('slime', '/assets/images/slime.png', {
-		width: 16,
-		height: 16,
-	}, 0, 2);
 
-	createSprite({
-		id: 'slime',
-		spriteFrames: [
-			'slime_0',
-			'slime_1',
-		],
-	});
-}
-
-function loadGiantSlimeSprite() {
-	loadSpriteFrames('giantslime', '/assets/images/giantslime.png', {
-		width: 32,
-		height: 32,
-	}, 0, 2);
-
-	createSprite({
-		id: 'giantslime',
-		spriteFrames: [
-			'giantslime_0',
-			'giantslime_1',
-		],
-	});
-}
-
-function loadWallSprite() {
-	loadSpriteFrames('wall', '/assets/images/wall.png', {
-		width: 16,
-		height: 16,
-	});
-
-	createSprite({
-		id: 'wall',
-		spriteFrames: [
-			'wall_0',
-		],
-	});
-}
-
-function loadGraveSprite() {
-	loadSpriteFrames('grave', '/assets/images/grave.png', {
-		width: 16,
-		height: 16,
-	});
-
-	createSprite({
-		id: 'grave',
-		spriteFrames: [
-			'grave_0',
-		],
-	});
-}
+export const spriteIndex = [
+	{
+		name: 'greenknight',
+		filepath: '/assets/images/greenknight.png',
+		frameSize: {
+			width: 16,
+			height: 16,
+		},
+		frameStart: 0,
+		frameOffset: 6,
+		frames: [0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 4, 5],
+	},
+	{
+		name: 'slime',
+		filepath: '/assets/images/slime.png',
+		frameSize: {
+			width: 16,
+			height: 16,
+		},
+		frameStart: 0,
+		frameOffset: 2,
+		frames: [0, 1],
+	},
+	{
+		name: 'giantslime',
+		filepath: '/assets/images/giantslime.png',
+		frameSize: {
+			width: 32,
+			height: 32,
+		},
+		frameStart: 0,
+		frameOffset: 2,
+		frames: [0, 1],
+	},
+	{
+		name: 'wall',
+		filepath: '/assets/images/giantslime.png',
+		frameSize: {
+			width: 16,
+			height: 16,
+		},
+		frameStart: 0,
+		frameOffset: 1,
+		frames: [0],
+	},
+	{
+		name: 'grave',
+		filepath: '/assets/images/grave.png',
+		frameSize: {
+			width: 16,
+			height: 16,
+		},
+		frameStart: 0,
+		frameOffset: 1,
+		frames: [0],
+	},
+	{
+		name: 'rustydagger',
+		filepath: '/assets/images/rustydagger.png',
+		frameSize: {
+			width: 16,
+			height: 16,
+		},
+		frameStart: 0,
+		frameOffset: 1,
+		frames: [0],
+	},
+]
