840ae7208ce081bd04b18735d0ea4c90da8a952f
Joeri Bakker <bakkerjoeri@gmail.com>
2018-11-25 23:31:43 +0100

    Add a Rusty Dagger

--- script/assets/loadSprites.js
+++ script/assets/loadSprites.js
@@ -1,8 +1,9 @@
+import sprites from '../../assets/sprites.js';
 import {loadSpriteFrames} from './../library/core/module/SpriteFrame.js';
 import {createSprite} from './../library/core/module/Sprite.js';
 
 export default function loadSprites() {
-	spriteIndex.forEach((spriteData) => {
+	sprites.forEach((spriteData) => {
 		loadSpriteFrames(spriteData.name, spriteData.filepath, spriteData.frameSize, spriteData.frameStart, spriteData.frameOffset);
 		createSprite({
 			id: spriteData.name,
@@ -10,73 +11,3 @@ export default function loadSprites() {
 		});
 	});
 }
-
-
-export const spriteIndex = [
-	{
-		name: 'greenknight',
-		filepath: '/assets/images/greenknight.png',
-		frameSize: {
-			width: 16,
-			height: 16,
-		},
-		frameStart: 0,
-		frameOffset: 6,
-		frames: [0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 4, 5],
-	},
-	{
-		name: 'slime',
-		filepath: '/assets/images/slime.png',
-		frameSize: {
-			width: 16,
-			height: 16,
-		},
-		frameStart: 0,
-		frameOffset: 2,
-		frames: [0, 1],
-	},
-	{
-		name: 'giantslime',
-		filepath: '/assets/images/giantslime.png',
-		frameSize: {
-			width: 32,
-			height: 32,
-		},
-		frameStart: 0,
-		frameOffset: 2,
-		frames: [0, 1],
-	},
-	{
-		name: 'wall',
-		filepath: '/assets/images/giantslime.png',
-		frameSize: {
-			width: 16,
-			height: 16,
-		},
-		frameStart: 0,
-		frameOffset: 1,
-		frames: [0],
-	},
-	{
-		name: 'grave',
-		filepath: '/assets/images/grave.png',
-		frameSize: {
-			width: 16,
-			height: 16,
-		},
-		frameStart: 0,
-		frameOffset: 1,
-		frames: [0],
-	},
-	{
-		name: 'rustydagger',
-		filepath: '/assets/images/rustydagger.png',
-		frameSize: {
-			width: 16,
-			height: 16,
-		},
-		frameStart: 0,
-		frameOffset: 1,
-		frames: [0],
-	},
-]
--- script/gameObjects/items/equipment/RustyDagger.js
+++ script/gameObjects/items/equipment/RustyDagger.js
@@ -1,8 +1,12 @@
 import Equipment from './../Equipment.js';
+import SpriteComponent from './../../../library/core/component/SpriteComponent.js';
 
 export const RUSTY_DAGGER_COMPONENTS_BLUEPRINT = {
 	name: 'Rusty dagger',
 	attack: 3,
+	sprite: new SpriteComponent({
+		assetId: 'rustydagger',
+	}),
 };
 
 export default function RustyDagger(components = {}) {
