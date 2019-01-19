69bbd90a0a047f9008f0da8681d94509c2ba975b
Joeri Bakker <bakkerjoeri@gmail.com>
2019-01-19 10:59:04 +0100

    Return state if there's no item to pick up

--- script/eventHandlers/pickUp.js
+++ script/eventHandlers/pickUp.js
@@ -18,25 +18,27 @@ export const pickUp = (state, gameObject, emitEvent) => {
 		state, currentLevelId, positionInLevel, sizeInLevel, [gameObject.id]
 	).find((gameObject) => { return gameObject.components.isItem });
 
-	if (itemToPickUp) {
-		// Move first item found into inventory
-		state = setComponentForGameObject(gameObject.id, 'inventory', [
-			...gameObject.components.inventory,
-			itemToPickUp.id,
-		])(state);
-
-		// Remove item from the level
-		state = removeGameObjectFromPositionInLevel(
-			state,
-			itemToPickUp,
-			itemToPickUp.components.currentLevelId,
-			itemToPickUp.components.positionInLevel
-		);
-
-		state = removeComponentFromGameObject(itemToPickUp.id, 'isVisible')(state);
-
-		emitEvent('log', {}, `${gameObject.components.name} picks up ${itemToPickUp.components.name}`);
-
-		return emitEvent('concludeTurn', state, gameObject);
+	if (!itemToPickUp) {
+		return state;
 	}
+
+	// Move first item found into inventory
+	state = setComponentForGameObject(gameObject.id, 'inventory', [
+		...gameObject.components.inventory,
+		itemToPickUp.id,
+	])(state);
+
+	// Remove item from the level
+	state = removeGameObjectFromPositionInLevel(
+		state,
+		itemToPickUp,
+		itemToPickUp.components.currentLevelId,
+		itemToPickUp.components.positionInLevel
+	);
+
+	state = removeComponentFromGameObject(itemToPickUp.id, 'isVisible')(state);
+
+	emitEvent('log', {}, `${gameObject.components.name} picks up ${itemToPickUp.components.name}`);
+
+	return emitEvent('concludeTurn', state, gameObject);
 }
